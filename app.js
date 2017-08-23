1f7d02efea38dea9f823d65c6a6aca542f8da849
emanuelurena <emanuelurena@mini-de-emanuel>
2017-08-22 21:13:13 -0600

    Merge origin/master
    Conflicts:
    	README.md

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var methodOverride = require('method-override');
 var errorhandler = require('errorhandler');
 
 //all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 //app.use(express.favicon());
